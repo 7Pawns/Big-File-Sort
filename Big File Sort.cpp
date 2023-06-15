5c923e9807d69a5f4daeac767a599f7a4a7188e7
7Pawns <124451776+7Pawns@users.noreply.github.com>
2023-06-15 17:25:14 +0300

    Added calculation of seg files amount and file read function

--- Big File Sort.cpp
+++ Big File Sort.cpp
@@ -1,10 +1,10 @@
 #include <Windows.h>
 #include <iostream>
 #include <exception>
+#include <vector>
 
 // Nicer looking messages
-const std::string prefix = "[+]";
-void prefixPrint(std::string text);
+const std::string prefix = "[+] ";
 
 class FileSort {
 private:
@@ -27,29 +27,70 @@ void FileSort::Sort(const std::string &inFilePath, const std::string &outFilePat
     DWORD dwFileAttrib = GetFileAttributes(wsInFilePath);
 
     if (!(dwFileAttrib != INVALID_FILE_ATTRIBUTES && !(dwFileAttrib & FILE_ATTRIBUTE_DIRECTORY))) {
-        std::cout << "File Doesn't Exist" << std::endl;
+        std::cout << prefix << "File Doesn't Exist" << std::endl;
         return;
     }
 
     // Check if file size exceed maxFileSizeBytes
-    HANDLE hFile = CreateFile(wsInFilePath, FILE_GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, NULL);
-    if (hFile == NULL) {
+    HANDLE hFile = CreateFile(wsInFilePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        std::cout << "Unable to open file" << std::endl;
         std::cout << GetLastError() << std::endl;
         return;
     }
 
     DWORD fileSize = GetFileSize(hFile, NULL);
-    std::cout << fileSize << std::endl;
+    if (fileSize > MaxFileSizeBytes) {
+        std::cout << prefix << "File size surpassed the maximum size supplied" << std::endl;
+    }
+    std::cout << prefix << "File size is: " << fileSize << std::endl;
 
+
+    // Create Diretory for segment files
+    if (!CreateDirectory(L"./segments", NULL)) {
+        // Error for already created directory
+        if (GetLastError() == 183) {
+            std::cout << prefix << "segments folder already created" << std::endl;
+        }
+        else {
+            std::cout << GetLastError() << std::endl;
+        }
+    }
+
+    // Opening Big File for read and write into segment files
+    DWORD nRead = 0;
+    std::vector<char> v(LineSizeBytes*NumberOfLinesPerSegment);
+    char *buff = &v[0];
+
+    // Check if there will be N full segments or N full segments and 1 that isn't full;
+    double intpart;
+    int segFileCount = modf((double)fileSize / (double)LineSizeBytes, &intpart) == 0.0 ? fileSize / LineSizeBytes : fileSize / LineSizeBytes + 1;
+    std::cout << prefix << "Amount of segment files created: " << segFileCount << std::endl;
+
+    // Read and write LineSizeBytes * NumberOfLinesPerSegment into each segment file
+    for (int i = 0; i < segFileCount; ++i) { }
+    if (!ReadFile(hFile, (LPVOID)buff, LineSizeBytes * NumberOfLinesPerSegment, &nRead, NULL)) {
+        std::cout << prefix << "ReadFile failed: " << GetLastError() << std::endl;
+    }
+    std::cout << prefix << "Read " << nRead << " bytes" << std::endl;
+    
 }
 
 int main(int argc, char **argv)
 {
-    FileSort fs(1, 2, 3);
-    fs.Sort("D:/test.txt", "D:/");
+    // TODO: Delete when done
+    /*
+    if (argc != 3) {
+        std::cout << "Crucial parameteres missing. Syntax: ./app <inPath> <outPath>" << std::endl;
+        return 1;
+    }
+    */
+     
+    // int maxFileSizeBytes, int numberOfLinesPerSegment, int lineSizeBytes
+    FileSort fs(100, 2, 6);
+    fs.Sort("tests/test1.txt", argv[0]);
+
+    return 0;
 }
 
-void prefixPrint(std::string text) {
-    std::cout << prefix << " " << text;
-}
 
